b2302a7da39c6faa037fde2f653f920caba3547d
MIhraiz <mohammad.a.ihraiz@gmail.com>
2018-05-02 10:14:11 +0300

    Implements the main method

--- geo/src/Lab/run.java
+++ geo/src/Lab/run.java
@@ -1,9 +1,28 @@
 package Lab;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class run {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the course code: ");
+		String code = input.nextLine();
+		
+		System.out.println("Enter the course name: ");
+		String name = input.nextLine();
+		
+		System.out.println("Enter the grades -it will exit if you enter 0-.");
+		ArrayList<Double> grades = new ArrayList<>();
+		input.nextLine();
+		double grade;
+		do {
+			grade = input.nextDouble();
+			grades.add(grade);
+		} while (grade != 0);
+		
+		grades.remove(grades.size()-1);
 
 	}
 
